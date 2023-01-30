63e70afe63f9e665ab83bad3e19030ce7a8a0f52
pamtel <pamelaakosahiante@gmail.com>
2023-01-30 22:15:53 +0100

    added data component

--- constants/theme.js
+++ constants/theme.js
@@ -45,19 +45,19 @@ export const SIZES = {
 
 export const FONTS = {
   largeTitle: {
-    fontFamily: 'Roboto',
+    fontFamily: 'System',
     fontSize: SIZES.largeTitle,
     lineHeight: 55,
   },
-  h1: {fontFamily: 'Roboto', fontSize: SIZES.h1, lineHeight: 36},
-  h2: {fontFamily: 'Roboto', fontSize: SIZES.h2, lineHeight: 30},
-  h3: {fontFamily: 'Roboto', fontSize: SIZES.h3, lineHeight: 22},
-  h4: {fontFamily: 'Roboto', fontSize: SIZES.h4, lineHeight: 22},
-  body1: {fontFamily: 'Roboto', fontSize: SIZES.body1, lineHeight: 36},
-  body2: {fontFamily: 'Roboto', fontSize: SIZES.body2, lineHeight: 30},
-  body3: {fontFamily: 'Roboto', fontSize: SIZES.body3, lineHeight: 22},
-  body4: {fontFamily: 'Roboto', fontSize: SIZES.body4, lineHeight: 22},
-  body5: {fontFamily: 'Roboto', fontSize: SIZES.body5, lineHeight: 22},
+  h1: {fontFamily: 'System', fontSize: SIZES.h1, lineHeight: 36},
+  h2: {fontFamily: 'System', fontSize: SIZES.h2, lineHeight: 30},
+  h3: {fontFamily: 'System', fontSize: SIZES.h3, lineHeight: 22},
+  h4: {fontFamily: 'System', fontSize: SIZES.h4, lineHeight: 22},
+  body1: {fontFamily: 'System', fontSize: SIZES.body1, lineHeight: 36},
+  body2: {fontFamily: 'System', fontSize: SIZES.body2, lineHeight: 30},
+  body3: {fontFamily: 'System', fontSize: SIZES.body3, lineHeight: 22},
+  body4: {fontFamily: 'System', fontSize: SIZES.body4, lineHeight: 22},
+  body5: {fontFamily: 'System', fontSize: SIZES.body5, lineHeight: 22},
 };
 
 const appTheme = {COLORS, SIZES, FONTS};
--- screens/Restaurant.tsx
+++ screens/Restaurant.tsx
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Image, Animated } from 'react-native'
+import { isIphoneX } from 'react-native-iphone-x-helper'
+import { icons, COLORS, SIZES, FONTS } from '../constants'
 
-export const Restaurant = () => {
+export const Restaurant = ({ route }: any) => {
+  const [restaurant, setRestaurant] = useState<null>(null)
+  const [currentLocation, setCurrentLocation] = useState<null>(null)
+
+  useEffect(() => {
+    let { item, currentLocation } = route.params;
+  }, [])
+
   return (
-    <View>
-      <Text>Restaurant</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* {renderHeader()} */}
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightGray2,
+  }
+})
