aeb6f43f9f1f8fa9de01e3f3ec9473f13cfd2df5
pamtel <pamelaakosahiante@gmail.com>
2023-01-30 13:17:24 +0100

    updated

--- constants/images.ts
+++ constants/images.ts
@@ -1,32 +1,32 @@
-const avatar_1: String = require('../assets/images/avatar-1.jpg');
-const avatar_2: String = require('../assets/images/avatar-2.jpg');
-const avatar_3: String = require('../assets/images/avatar-3.jpg');
-const avatar_4: String = require('../assets/images/avatar-4.jpg');
-const avatar_5: String = require('../assets/images/avatar-5.jpg');
-const baked_fries: String = require('../assets/images/baked-fries.jpg');
-const burger_restaurant_1: String = require('../assets/images/burger-restaurant.jpg');
-const burger_restaurant_2: String = require('../assets/images/burger-restaurant-2.jpg');
-const chicago_hot_dog: String = require('../assets/images/chicago-hot-dog.jpg');
-const crispy_chicken_burger: String = require('../assets/images/crispy-chicken-burger.jpg');
-const fries_restaurant: String = require('../assets/images/fries-restaurant.jpg');
-const hawaiian_pizza: String = require('../assets/images/hawaiian-pizza.jpg');
-const honey_mustard_chicken_burger: String = require('../assets/images/honey-mustard-chicken-burger.jpg');
-const hot_dog_restaurant: String = require('../assets/images/hot-dog-restaurant.jpg');
-const ice_kacang: String = require('../assets/images/ice-kacang.jpg');
-const japanese_restaurant: String = require('../assets/images/japanese-restaurant.jpg');
-const kek_lapis_shop: String = require('../assets/images/kek-lapis-shop.jpg');
-const kek_lapis: String = require('../assets/images/kek-lapis.jpg');
-const kolo_mee: String = require('../assets/images/kolo-mee.jpg');
-const nasi_briyani_mutton: String = require('../assets/images/nasi-briyani-mutton.jpg');
-const nasi_lemak: String = require('../assets/images/nasi-lemak.jpg');
-const noodle_shop: String = require('../assets/images/noodle-shop.jpg');
-const pizza_restaurant: String = require('../assets/images/pizza-restaurant.jpg');
-const pizza: String = require('../assets/images/pizza.jpg');
-const salad: String = require('../assets/images/salad.jpg');
-const sarawak_laksa: String = require('../assets/images/sarawak-laksa.jpg');
-const sushi: String = require('../assets/images/sushi.jpg');
-const teh_c_peng: String = require('../assets/images/teh-c-peng.jpg');
-const tomato_pasta: String = require('../assets/images/tomato-pasta.jpg');
+const avatar_1: Object = require('../assets/images/avatar-1.jpg');
+const avatar_2: Object = require('../assets/images/avatar-2.jpg');
+const avatar_3: Object = require('../assets/images/avatar-3.jpg');
+const avatar_4: Object = require('../assets/images/avatar-4.jpg');
+const avatar_5: Object = require('../assets/images/avatar-5.jpg');
+const baked_fries: Object = require('../assets/images/baked-fries.jpg');
+const burger_restaurant_1: Object = require('../assets/images/burger-restaurant.jpg');
+const burger_restaurant_2: Object = require('../assets/images/burger-restaurant-2.jpg');
+const chicago_hot_dog: Object = require('../assets/images/chicago-hot-dog.jpg');
+const crispy_chicken_burger: Object = require('../assets/images/crispy-chicken-burger.jpg');
+const fries_restaurant: Object = require('../assets/images/fries-restaurant.jpg');
+const hawaiian_pizza: Object = require('../assets/images/hawaiian-pizza.jpg');
+const honey_mustard_chicken_burger: Object = require('../assets/images/honey-mustard-chicken-burger.jpg');
+const hot_dog_restaurant: Object = require('../assets/images/hot-dog-restaurant.jpg');
+const ice_kacang: Object = require('../assets/images/ice-kacang.jpg');
+const japanese_restaurant: Object = require('../assets/images/japanese-restaurant.jpg');
+const kek_lapis_shop: Object = require('../assets/images/kek-lapis-shop.jpg');
+const kek_lapis: Object = require('../assets/images/kek-lapis.jpg');
+const kolo_mee: Object = require('../assets/images/kolo-mee.jpg');
+const nasi_briyani_mutton: Object = require('../assets/images/nasi-briyani-mutton.jpg');
+const nasi_lemak: Object = require('../assets/images/nasi-lemak.jpg');
+const noodle_shop: Object = require('../assets/images/noodle-shop.jpg');
+const pizza_restaurant: Object = require('../assets/images/pizza-restaurant.jpg');
+const pizza: Object = require('../assets/images/pizza.jpg');
+const salad: Object = require('../assets/images/salad.jpg');
+const sarawak_laksa: Object = require('../assets/images/sarawak-laksa.jpg');
+const sushi: Object = require('../assets/images/sushi.jpg');
+const teh_c_peng: Object = require('../assets/images/teh-c-peng.jpg');
+const tomato_pasta: Object = require('../assets/images/tomato-pasta.jpg');
 
 export default {
   avatar_1,
@@ -55,6 +55,7 @@ export default {
   noodle_shop,
   pizza_restaurant,
   pizza,
+  salad,
   sarawak_laksa,
   sushi,
   teh_c_peng,
--- navigation/Tabs.tsx
+++ navigation/Tabs.tsx
@@ -2,64 +2,171 @@ import React from 'react'
 import { View, TouchableOpacity, Image, StyleSheet } from 'react-native'
 import { createBottomTabNavigator, BottomTabBar } from '@react-navigation/bottom-tabs'
 import { Home } from "../screens"
-import { COLORS, icons } from '../constants'
+import { icons, COLORS } from '../constants'
+import Svg, { Path } from 'react-native-svg'
+import { isIphoneX } from 'react-native-iphone-x-helper'
+
+interface ITabBarCustomButton {
+  accessibilityState: { selected: boolean }
+  children: any;
+  onPress: () => void
+}
+
+interface IBottomTab {
+  props: any;
+}
 
 const Tab = createBottomTabNavigator()
 
-const Tabs: React.FC = () => {
+const TabBarCustomButton: React.FC<ITabBarCustomButton> = ({ accessibilityState, children, onPress }) => {
+  var isSelected = accessibilityState.selected
+
+  if (isSelected) {
+    return (
+      <View style={{ flex: 1, alignItems: "center" }}>
+        <View style={{ flexDirection: 'row', position: 'absolute', top: 0 }}>
+          <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
+          <Svg
+            width={70}
+            height={61}
+            viewBox="0 0 75 61"
+          >
+            <Path
+              d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+              fill={COLORS.white}
+            />
+          </Svg>
+          <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            top: -22.5,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            backgroundColor: COLORS.white
+          }}
+          onPress={onPress}
+        >
+          {children}
+        </TouchableOpacity>
+      </View>
+    )
+  } else {
+    return (
+      <TouchableOpacity
+        style={{
+          flex: 1,
+          height: 60,
+          backgroundColor: COLORS.white
+        }}
+        activeOpacity={1}
+        onPress={onPress}
+      >
+        {children}
+      </TouchableOpacity>
+    )
+  }
+}
+
+const CustomTabBar = (props: IBottomTab) => {
+  if (isIphoneX()) {
+    return (
+      <View>
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: 30,
+            backgroundColor: COLORS.white
+          }}
+        ></View>
+        <BottomTabBar
+          {...props.props}
+        />
+      </View>
+    )
+  } else {
+    return (
+      <BottomTabBar
+        {...props.props}
+      />
+    )
+  }
+}
+
+const Tabs = () => {
   return (
-    <Tab.Navigator screenOptions={{
-      tabBarShowLabel: false, tabBarStyle: {
-        position: 'absolute',
-        left: 0,
-        bottom: 0,
-        right: 0,
-        borderTopWidth: 0,
-        backgroundColor: "transparent",
-        elevation: 0
-      }
-    }}>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle: {
+          position: 'absolute',
+          left: 0,
+          bottom: 0,
+          right: 0,
+          borderTopWidth: 0,
+          backgroundColor: "transparent",
+          elevation: 0
+        }
+      }}
+      tabBar={(props) => (
+        <CustomTabBar
+          props={props}
+        />
+      )}
+    >
       <Tab.Screen name="HomeScreen" component={Home} options={{
         tabBarIcon: ({ focused }) => (
           <Image
-            source={require('../assets/icons/cutlery.png')}
+            source={icons.cutlery}
             resizeMode="contain"
             style={[styles.tab, { tintColor: focused ? COLORS.primary : COLORS.secondary }]}
           />
-        )
+        ),
+        tabBarButton: (props) => <TabBarCustomButton {...props as ITabBarCustomButton} />
       }}
       />
       <Tab.Screen name="Search" component={Home} options={{
         tabBarIcon: ({ focused }) => (
           <Image
-            source={require('../assets/icons/search.png')}
+            source={icons.search}
             resizeMode="contain"
             style={[styles.tab, { tintColor: focused ? COLORS.primary : COLORS.secondary }]}
           />
-        )
+        ),
+        tabBarButton: (props) => <TabBarCustomButton  {...props as ITabBarCustomButton} />
       }}
       />
       <Tab.Screen name="Like" component={Home} options={{
         tabBarIcon: ({ focused }) => (
           <Image
-            source={require('../assets/icons/like.png')}
+            source={icons.like}
             resizeMode="contain"
             style={[styles.tab, { tintColor: focused ? COLORS.primary : COLORS.secondary }]}
           />
-        )
+        ),
+        tabBarButton: (props) => <TabBarCustomButton {...props as ITabBarCustomButton} />
       }}
       />
       <Tab.Screen name="User" component={Home} options={{
         tabBarIcon: ({ focused }) => (
           <Image
-            source={require('../assets/icons/user.png')}
+            source={icons.user}
             resizeMode="contain"
             style={[styles.tab, { tintColor: focused ? COLORS.primary : COLORS.secondary }]}
           />
-        )
+        ),
+        tabBarButton: (props) => <TabBarCustomButton  {...props as ITabBarCustomButton} />
       }}
       />
-    </Tab.Navigator>
+    </Tab.Navigator >
   )
 }
 
